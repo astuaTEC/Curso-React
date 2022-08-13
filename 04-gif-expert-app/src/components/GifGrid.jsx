8abeb1f9bba4e8a3a62e51bc22b9633cb02b97cd
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2022-08-12 18:35:39 -0600

    Seccion 6 terminada

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,13 +1,31 @@
-import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GrifItem } from "./GrifItem";
 
 export const GifGrid = ({ category }) => {
 
-    getGifs( category );
+    const { images, isLoading } = useFetchGifs( category );
+
+
 
     return (
         <>
-            <h3>{ category }</h3>
-            <p>Hola mundo</p>
+            <h3>{category}</h3>
+
+            {
+                isLoading && ( <h2>Cargando...</h2> )
+            }
+
+            <div className="card-grid">
+                {
+                    images.map( (img) => (
+                        <GrifItem 
+                            key={ img.id }
+                            { ...img }
+                        />
+                    ))
+                }
+            </div>
+
         </>
     )
 }
--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 
 export const getGifs = async( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=MnLo8SwZF8W3FTjr44WCYXyXtDykjI4X&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=MnLo8SwZF8W3FTjr44WCYXyXtDykjI4X&q=${category}&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
@@ -12,8 +12,6 @@ export const getGifs = async( category ) => {
             url: img.images.downsized_medium.url
         }
     ));
-
-    console.log(gifs);
 
     return gifs;
 }
