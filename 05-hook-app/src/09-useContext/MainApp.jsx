4a4801b8dae67ac5f085d9ab44f965a360ef410b
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2022-12-15 20:22:57 -0600

    Seccion 11 terminada

---
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { UserProvider } from './context/UserProvider'
+import { Navbar } from './Navbar'
+
+export const MainApp = () => {
+    return (
+        <UserProvider>
+            <h1>MainApp</h1>
+            <Navbar />
+
+            <hr />
+
+            <Outlet />
+        </UserProvider>
+    )
+}
