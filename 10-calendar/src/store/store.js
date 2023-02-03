7abc4a41d716ffd0537f0c0c6acdd9a021067dbe
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2023-02-02 21:30:32 -0600

    Seccion 26 terminada

--- 10-calendar/src/store/store.js
+++ 10-calendar/src/store/store.js
@@ -1,14 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { calendarSlice, uiSlice } from "./";
+import { authSlice, calendarSlice, uiSlice } from "./";
 
 
 export const store = configureStore({
+    reducer: {
+        auth: authSlice.reducer,
+        ui: uiSlice.reducer,
+        calendar: calendarSlice.reducer,
+    },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware({
             serializableCheck: false,
-        }),
-    reducer: {
-        ui: uiSlice.reducer,
-        calendar: calendarSlice.reducer
-    }
+        })
 })
